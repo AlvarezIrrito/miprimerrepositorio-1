e82bfe086f9d768a31485506ba87de4bd059772f
marcefotos75 <marcefotos75@gmail.com>
2021-07-29 20:00:38 -0300

    Cambio en Linea 6

--- classes.py
+++ classes.py
@@ -3,6 +3,7 @@
 import pyodbc
 from db import connect, create_table, get_data, get_all_data, update_data, insert_data
 
+# cambio en linea seis de Leguizamon
 
 # En este archivo definimos las clases asociadas a la solución del problema
 
